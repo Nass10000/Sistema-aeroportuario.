7ec70722ca6825ed0fc65773c7d9af18793545a2
Nass10000 <nassimwhazim@gmail.com>
2025-06-30 14:42:09 -0400

    actualizacion

--- backend/src/assignment/assignment.module.ts
+++ backend/src/assignment/assignment.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Assignment } from './assignment.entity';
 import { AssignmentService } from './assignment.service';
 import { AssignmentController } from './assignment.controller';
+import { NotificationModule } from '../notification/notification.module'; // <-- Importa el módulo
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Assignment])],
+  imports: [
+    TypeOrmModule.forFeature([Assignment]),
+    NotificationModule, // <-- Agrégalo aquí
+  ],
   providers: [AssignmentService],
   controllers: [AssignmentController],
   exports: [AssignmentService],
